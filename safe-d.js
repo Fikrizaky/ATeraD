d497df3db565370795dd835f1611c61cee1da2c2
Fikrizaky <fikrizaky2711@gmail.com>
2018-01-23 19:30:17 +0700

    Create safe-d.js

---
+++ safe-d.js
@@ -0,0 +1,5 @@
+var safelink = {
+  converter: "https://safe-downloading.blogspot.com/p/wait.html,https://safe-downloading.blogspot.com/p/hold.html,https://safe-downloading.blogspot.com/p/load.html,https://safe-downloading.blogspot.com/p/here.html,https://safe-downloading.blogspot.com/p/go.html", // Link Converter
+  link: "drive.google.com,mega.nz,sendit.cloud,clicknupload.org,downace.com", // Masukan link yang ingin di convert otomatis.
+  password: '' // Masukan password atau kosongkan saja
+}
